4df74d707a3df5ab93e501343adbe7faf30f8b1a
black-s0s3 <ebenezer.nyarko@amalitech.org>
2020-10-06 23:56:27 +0800

    a new array was created in myapp.js and modified

--- myapp.js
+++ myapp.js
@@ -1,11 +1,13 @@
-var car = {
-  color: "red",
-  speed: 20,
-  drive: function(){return "drive";}
-};
-
-var shoppinglist = [
-  "mango",
-  "apple",
-  "orange"
+var array = [
+    "string",
+    "hello",
+    "world200",
+    200,
+    function(){ return "drive"; },
 ];
+array[0] = "New string";
+array.shift();
+array.pop();
+array.unshift( "new", "values" );
+array.push( "ending", "values" );
+array.splice( 2, 2, "new elemets", [], {}, 200, 300, function(){} );

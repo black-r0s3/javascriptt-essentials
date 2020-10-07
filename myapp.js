7ac30782527ecba2888ce92692d76faf2c77154b
black-s0s3 <ebenezer.nyarko@amalitech.org>
2020-10-07 13:39:40 +0800

    tageting object elements and use of appendchild on elements

--- myapp.js
+++ myapp.js
@@ -1,7 +1,11 @@
-var classRegister = [ "Lawrence", "John", "Jeff" ];
+var element = document.createElement('div');
 
+element.style.cssText = "width:200px; height:20px; background:blue;";
 
-for( var index in classRegister )
-{
-    console.log( classRegister[ index ] );
-}
+element.onclick = function(){ alert('hello'); };
+
+document.body.appendChild( element );
+
+var target = document.getElementById('yellow');
+
+document.body.insertAfter( element, target );

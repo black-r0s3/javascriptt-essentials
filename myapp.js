46705b5f301270b1e63e755417952aaa96958a7b
black-s0s3 <ebenezer.nyarko@amalitech.org>
2020-10-07 11:25:59 +0800

    a constructor called Apple was created and instances of it was also created

--- myapp.js
+++ myapp.js
@@ -1,13 +1,11 @@
-var array = [
-    "string",
-    "hello",
-    "world200",
-    200,
-    function(){ return "drive"; },
-];
-array[0] = "New string";
-array.shift();
-array.pop();
-array.unshift( "new", "values" );
-array.push( "ending", "values" );
-array.splice( 2, 2, "new elemets", [], {}, 200, 300, function(){} );
+function Apple( x, y, color, score )
+{
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.score = score;
+}
+
+var apple1 = new Apple( 5, 10, "blue", 200 );
+var apple2 = new Apple( 50, 100, "red", 25);
+var apple3 = new Apple( 20, 200, "black", 10 );
